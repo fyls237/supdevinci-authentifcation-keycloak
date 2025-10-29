aab4f07d1e8f8352d77277e10ce842f9f0f8f26d
fyls237 <loisteve27@gmail.com>
2025-10-29 22:58:27 +0000

    feat: add compute module for keycloak

---
+++ infrastructure/modules/compute/outputs.tf
@@ -0,0 +1,19 @@
+output "instance_id" {
+  description = "The ID of the Keycloak EC2 instance"
+  value       = aws_instance.keycloak.id
+}
+
+output "instance_public_ip" {
+  description = "The public IP address of the Keycloak EC2 instance"
+  value       = aws_instance.keycloak.public_ip
+}
+
+output "security_group_id" {
+  description = "The ID of the Keycloak EC2 instance security group"
+  value       = aws_security_group.keycloak_sg.id
+}
+
+output "security_group_ingress_rules" {
+  description = "The ingress rules of the Keycloak EC2 instance security group"
+  value       = aws_security_group.keycloak_sg.ingress
+}
