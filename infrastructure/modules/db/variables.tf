8935a3315e24e4c39fcc5dd8f6159f79e31d4890
fyls237 <loisteve27@gmail.com>
2025-10-29 23:39:24 +0000

    feat: add postresql module

---
+++ infrastructure/modules/db/variables.tf
@@ -0,0 +1,102 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "name_prefix" {
+  description = "Prefix for resource names"
+  type        = string
+}
+
+variable "tags" {
+  description = "Tags to apply to all resources"
+  type        = map(string)
+  default     = {}
+}
+
+variable "vpc_id" {
+  description = "ID of the VPC"
+  type        = string
+}
+
+variable "private_subnet_ids" {
+  description = "List of private subnet IDs for DB subnet group"
+  type        = list(string)
+}
+
+variable "db_instance_class" {
+  description = "RDS instance class"
+  type        = string
+  default     = "db.t3.medium"
+}
+
+variable "db_engine" {
+  description = "Database engine"
+  type        = string
+  default     = "postgres"
+}
+
+variable "db_engine_version" {
+  description = "Database engine version"
+  type        = string
+  default     = "13.7"
+}
+
+variable "db_name" {
+  description = "Initial database name"
+  type        = string
+  default     = "appdb"
+}
+
+variable "db_username" {
+  description = "Master username"
+  type        = string
+}
+
+variable "db_password" {
+  description = "Master password"
+  type        = string
+  sensitive   = true
+}
+
+variable "allocated_storage" {
+  description = "Storage allocated (GB)"
+  type        = number
+  default     = 20
+}
+
+variable "backup_retention_period" {
+  description = "Backup retention days"
+  type        = number
+  default     = 7
+}
+
+variable "multi_az" {
+  description = "Multi-AZ deployment"
+  type        = bool
+  default     = false
+}
+
+variable "publicly_accessible" {
+  description = "Whether the DB is publicly accessible"
+  type        = bool
+  default     = false
+}
+
+variable "skip_final_snapshot" {
+  description = "Skip final snapshot on delete"
+  type        = bool
+  default     = true
+}
+
+
+variable "allowed_cidrs" {
+  description = "List of allowed CIDR blocks"
+  type        = list(string)
+}
+
+variable "max_connections" {
+  description = "Maximum database connections"
+  type        = number
+  default     = 100
+}
