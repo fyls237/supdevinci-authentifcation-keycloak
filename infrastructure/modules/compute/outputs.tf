c61ebc3ad7e799553fcfa67f727310b05161fa15
fyls237 <loisteve27@gmail.com>
2025-11-02 14:41:41 +0000

    feat: deploy infrastrucutre terraform

--- infrastructure/modules/compute/outputs.tf
+++ infrastructure/modules/compute/outputs.tf
@@ -17,3 +17,8 @@ output "security_group_ingress_rules" {
   description = "The ingress rules of the Keycloak EC2 instance security group"
   value       = aws_security_group.keycloak_sg.ingress
 }
+
+output "instance_private_ip" {
+  description = "The private IP address of the Keycloak EC2 instance"
+  value       = aws_instance.keycloak.private_ip
+}
---
+++ infrastructure/variables.tf
@@ -0,0 +1,75 @@
+variable "region" {
+  description = "AWS region"
+  type        = string  
+}
+
+variable "tags" {
+  description = "Map of tags to assign to resources"
+  type        = map(string)
+}
+
+variable "name_prefix" {
+  description = "Prefix for resource names"
+  type        = string
+}
+
+# Network Configuration
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of CIDR blocks for public subnets"
+  type        = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  description = "List of CIDR blocks for private subnets"
+  type        = list(string)
+}
+
+variable "azs" {
+  description = "List of availability zones"
+  type        = list(string)
+}
+
+# Storage Configuration
+variable "bucket_prefix" {
+    description = "Prefix for the S3 bucket name"
+    type        = string
+}
+
+# Compute Configuration
+variable "keycloak_ami_id" {
+  description = "AMI ID for the Keycloak EC2 instance"
+  type        = string
+}
+
+variable "keycloak_instance_type" {
+  description = "Instance type for the Keycloak EC2 instance"
+  type        = string
+}
+
+variable "keycloak_user_data" {
+  description = "User data for the Keycloak EC2 instance"
+  type        = string
+  default     = ""
+}
+
+variable "allowed_cidrs" {
+  description = "List of allowed CIDR blocks for the Keycloak EC2 instance"
+  type        = list(string)
+}
+
+variable "db_name" {
+  description = "Data base name for keycloak"
+  type =  string
+  default = "keycloakdb"
+}
+
+
+variable "profile" {
+  description = "Profile name to use for AWS provider"
+  type        = string
+}
